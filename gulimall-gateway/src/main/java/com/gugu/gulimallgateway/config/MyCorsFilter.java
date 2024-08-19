8e991e572b0d010c56dd8f8abd61efe7f5b3dddf
Gwynliu7 <2249012464@qq.com>
2024-08-20 00:55:02 +0800

    项目初始结构+3

---
+++ gulimall-gateway/src/main/java/com/gugu/gulimallgateway/config/MyCorsFilter.java
@@ -0,0 +1,28 @@
+package com.gugu.gulimallgateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * @author Gwynliu7
+ * @date 2024/8/19&20:08
+ */
+@Configuration
+public class MyCorsFilter {
+    @Bean
+    public CorsFilter corsFilter(){
+        //配置跨域
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOriginPattern("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**",corsConfiguration);
+        return new CorsFilter(source);
+    }
+}
--- gulimall-product/src/main/java/com/gugu/gulimall/product/controller/CategoryController.java
+++ gulimall-product/src/main/java/com/gugu/gulimall/product/controller/CategoryController.java
@@ -1,6 +1,7 @@
 package com.gugu.gulimall.product.controller;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,13 +32,13 @@ public class CategoryController {
     private CategoryService categoryService;
 
     /**
-     * 列表
+     * 查出所有分类以及子分类，以树形结构组装起来
      */
-    @RequestMapping("/list")
-    public R list(@RequestParam Map<String, Object> params){
-        PageUtils page = categoryService.queryPage(params);
+    @RequestMapping("/list/tree")
+    public R list(){
+        List<CategoryEntity> entities = categoryService.listWithTree();
 
-        return R.ok().put("page", page);
+        return R.ok().put("page", entities);
     }
 
 
@@ -73,6 +74,8 @@ public class CategoryController {
 
     /**
      * 删除
+     * @RequestBody 获取请求体，必须发送POST请求
+     * SpringMVC自动将请求体的数据（json），转为对应的对象
      */
     @RequestMapping("/delete")
     public R delete(@RequestBody Long[] catIds){
